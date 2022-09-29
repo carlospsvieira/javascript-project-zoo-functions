23dda77d5ed41d4c367e432ff9539bca167bf032
Carlos Vieira <carlepsvieira@gmail.com>
2022-09-29 16:12:11 -0300

    requirement 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { residents } = species.find((group) => group.name === animal);
+  return residents.every((it) => it.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
