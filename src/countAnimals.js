f4f0724f82f6165b0b9a025d2f0879c666c3e94f
Carlos Vieira <carlepsvieira@gmail.com>
2022-09-30 19:45:52 -0300

    requirement 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// ------- console tests ---------------- //
+// Return total number of animals by species if there isn't any parameters //
+// It has to be an object with species name as key and total number (length) as its value //
+// if (!animal) {
+//   const speeecies = species.reduce((acc, specie) =>
+//     ({ ...acc, [specie.name]: specie.residents.length }), {});
+//   console.log(speeecies);
+// }
+// if parameter is an object with key(specie)and value(specie's name) eg: {specie: 'lion'} returns the residents.length
+// const { residents } = species.find(({ name }) => name === animal.specie);
+// return residents.length
+// if ('sex' in animal) return residents.filter(({sex}) => sex === animal.sex).length;
+// -------------------------------------- //
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, specie) =>
+      ({ ...acc, [specie.name]: specie.residents.length }), {});
+  }
+  const { residents } = species.find(({ name }) => name === animal.specie);
+  if ('sex' in animal) {
+    return residents.filter(({ sex }) => sex === animal.sex).length;
+  }
+  return residents.length;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,19 +1,20 @@
 const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
-const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
-
 // --------- console tests ------------- //
-// verify whether a person is a manager or not (return true if any (some) are true)//
+// const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+// const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+// const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
+// Verify whether a person is a manager or not (return true if any (some) are true)//
 // const managersss = employees.some(({ managers }) => managers.includes('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 // console.log(managersss);
 // ----------------------------- //
-// if isManager is true return a list of employees working under this managaner //
+// If isManager is true return a list of employees working under this managaner //
 // const workingUnder = employees.filter(({ managers }) => managers.includes(managerId))
 // console.log(workingUner);
-// after finding these employees return first and last name in one list (array)//
+
+// After finding these employees return first and last name in one list (array)//
 // workingUnder.map((employee) => `${employee.firstName} ${employee.lastName}`);
 // ------------------------------- //
 
