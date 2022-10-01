31a90f02a3a8d0866c8a531081f26942a46706e6
Carlos Vieira <carlepsvieira@gmail.com>
2022-10-01 18:49:55 -0300

    requirement 8 done

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,3 +1,4 @@
+const en = require('faker/lib/locales/en');
 const data = require('../data/zoo_data');
 
 // ----------- console tests ------------ //
@@ -5,28 +6,20 @@ const data = require('../data/zoo_data');
 // .reduce will count how many ages appear and separate them in new obj
 // new obj has to receive new keys child, adult and senior
 // separate child, adult and senior in keys with total number of times they appear. //
+const entryees = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 
-// const entryees = [
-//   { name: 'Lara Carvalho', age: 5 },
-//   { name: 'Frederico Moreira', age: 5 },
-//   { name: 'Pedro Henrique Carvalho', age: 5 },
-//   { name: 'Maria Costa', age: 18 },
-//   { name: 'Núbia Souza', age: 18 },
-//   { name: 'Carlos Nogueira', age: 50 },
-// ];
-
-// const anything = entryees.reduce((acc, person) => {
-//   if (person.age < 18) acc.child += 1;
-//   if (person.age >= 18 && person.age < 50) acc.adult += 1;
-//   if (person.age >= 50) acc.senior += 1;
-//   return acc;
-// }, {
-//   child: 0,
-//   adult: 0,
-//   senior: 0,
-// });
-
-// console.log(anything);
+// if (person.includes('child')) acc.child *= 20.99;
+// if (person.includes('adult')) acc.adult *= 20.99;
+// if (person.includes('senior')) acc.senior *= 20.99;
+// ---------------------------------- //
+//
 
 function countEntrants(entrants) {
   return entrants.reduce((acc, person) => {
@@ -42,7 +35,10 @@ function countEntrants(entrants) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const { child, adult, senior } = countEntrants(entrants);
+  const totalCost = (child * 20.99) + (adult * 49.99) + (senior * 24.99);
+  return totalCost;
 }
 
 module.exports = { calculateEntry, countEntrants };
