cff03e6d59f80f2e0e7eb236bcd00088facd5b4d
Carlos Vieira <carlepsvieira@gmail.com>
2022-10-03 15:42:24 -0300

    half done requirement 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,37 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// ------------ console tests ------------- //
+// Species is an array of objects. Name is a key: 'string'. Availability is a key: array.
+// Hours is an object. Days are keys: + obj with 2 keys (open and close)
+
+// Return this first:
+// const obj = {
+//   Tuesday: {
+//     officeHour: 'Open from 8am until 6pm', // `Open from ${hours[day].open} until ${hours[day]close}`
+// ------ >> Which animal (species.name) is in exhibition on that day ???
+//       exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'], // species.availability
+//   },
+//
+// --------------------------------------- //
+
+// console.log(Object.keys(hours));
+species.at();
+function createSchedule(scheduleTarget) {
+  const day = Object.keys(hours).filter((jour) => jour.includes(scheduleTarget));
+  // create Object(table of content) //
+  const obj = {
+    [day]: {
+      officeHour: `Open from ${hours.open}am until ${hours.close}pm`,
+      // exhibition:
+    },
+  };
+  return obj[scheduleTarget];
+}
+
+console.log(createSchedule('Tuesday'));
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
 }
 
 module.exports = getSchedule;
