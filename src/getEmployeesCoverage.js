4317d5728fb1c82779e456a2078e2ac4a1983be0
Carlos Vieira <carlepsvieira@gmail.com>
2022-10-04 17:31:29 -0300

    requirement 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,8 +2,8 @@ const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 // -------------- tests ----------------------//
-const byName = { name: 'Sharonda' };
-const byId = { id: 'c1f50212-35a6-4ecd-8223-f835538526c2' };
+// const byName = { name: 'Sharonda' };
+// const byId = { id: 'c1f50212-35a6-4ecd-8223-f835538526c2' };
 // Return this:
 // {
 // id: "4b40a139-d4dc-4f09-822d-ec25e819a5ad", // id da pessoa
@@ -19,28 +19,45 @@ const byId = { id: 'c1f50212-35a6-4ecd-8223-f835538526c2' };
 //   species: employee.responsibleFor,
 //   locations: [employee.responsibleFor.map((specie) => specie.location)],
 // };
-// console.log(employeeByNameorID);
 // --------------------------------------------//
 
-function getEmployeesCoverage(employee) {
-  if (!employee) return employees.map((person) => )
-  const findEmployee = employees.reduce((acc, person) => {
-    if (person.firstName.includes(employee.name)) return person;
-    if (person.lastName.includes(employee.name)) return person;
-    if (person.id.includes(employee.id)) return person;
-    return acc;
-  });
-  const animals = species.filter((specie) =>
-    findEmployee.responsibleFor.includes(specie.id));
+// const findEmployee = (employee) =>
+//   employees.reduce((acc, person) => {
+//     if (person.firstName.includes(employee.name)) return person;
+//     if (person.lastName.includes(employee.name)) return person;
+//     if (person.id.includes(employee.id)) return person;
+//     return acc;
+//   }); ------->> is async function, so it doesn't work when throwing errors.
 
-  const employeeCoverage = {
-    id: findEmployee.id,
-    fullName: `${findEmployee.firstName} ${findEmployee.lastName}`,
-    species: animals.map((animal) => animal.name),
-    locations: animals.map((animal) => animal.location),
-  };
-  return employeeCoverage;
+// 'function call' works for throwing errors (src: MDN) //
+
+const findEmployee = (employee) => {
+  if (employee.name) {
+    return employees.find((person) =>
+      person.firstName.includes(employee.name) || person.lastName.includes(employee.name));
+  }
+  if (employee.id) {
+    return employees.find((person) => person.id.includes(employee.id));
+  }
+};
+
+const employeeInfo = (employee) => ({
+  id: employee.id,
+  fullName: `${employee.firstName} ${employee.lastName}`,
+  species: species.filter((specie) =>
+    employee.responsibleFor.includes(specie.id)).map((animal) => animal.name),
+  locations: species.filter((specie) =>
+    employee.responsibleFor.includes(specie.id)).map((animal) => animal.location),
+});
+
+function getEmployeesCoverage(employee) {
+  // returns all employees' info in case there's no parameter //
+  if (!employee) return employees.map((person) => employeeInfo(person));
+  // throw an error in case the parameter does not exist in data //
+  if (!findEmployee(employee)) throw new Error('Informações inválidas');
+  // returs wanted employee's info ----->> //
+  return employeeInfo(findEmployee(employee));
 }
 
-console.log(getEmployeesCoverage(byName));
+// console.log(getEmployeesCoverage(byName));
 module.exports = getEmployeesCoverage;
