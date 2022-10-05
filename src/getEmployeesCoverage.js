72ca6e77ad61e95937d1045fbc13984c679e7d84
Carlos Vieira <carlepsvieira@gmail.com>
2022-10-05 12:00:43 -0300

    modified:   src/getEmployeesCoverage.js 	modified:   src/getSchedule.js

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -4,21 +4,6 @@ const data = require('../data/zoo_data');
 // -------------- tests ----------------------//
 // const byName = { name: 'Sharonda' };
 // const byId = { id: 'c1f50212-35a6-4ecd-8223-f835538526c2' };
-// Return this:
-// {
-// id: "4b40a139-d4dc-4f09-822d-ec25e819a5ad", // id da pessoa
-// fullName: "Sharonda Spry", // nome completo: firstName + lastName
-// species: [ "otters", "frogs" ], // espécies as quais a pessoa é responsável
-// locations: [ "SE", "SW" ], // Um array contendo todas as localizações das espécies
-// }
-//
-// function will receive an obj as parameter. {id: - } or {name: - }
-// const obj = {
-//   id: employee.id,
-//   fullName: `${employee.firstName} ${employee.lastName}`,
-//   species: employee.responsibleFor,
-//   locations: [employee.responsibleFor.map((specie) => specie.location)],
-// };
 // --------------------------------------------//
 
 // const findEmployee = (employee) =>
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,11 +1,12 @@
 // const { species, hours } = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 // ------------ console tests ------------- //
 // Species is an array of objects. Name is a key: 'string'. Availability is a key: array.
 // Hours is an object. Days are keys: + obj with 2 keys (open and close)
 
-// Return this first:
+// Return this:
 // const obj = {
 //   Tuesday: {
 //     officeHour: 'Open from 8am until 6pm', // `Open from ${hours[day].open} until ${hours[day]close}`
@@ -14,8 +15,64 @@ const data = require('../data/zoo_data');
 //   },
 //
 // --------------------------------------- //
+const { Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday } = hours;
 
-function getSchedule(scheduleTarget) {
+const schedule = {
+  Tuesday: {
+    officeHour: `Open from ${Tuesday.open}am until ${Tuesday.close}pm`,
+    exhibition: species.reduce((acc, specie) => {
+      if (specie.availability.includes('Tuesday')) acc.push(specie.name);
+      return acc;
+    }, []),
+  },
+  Wednesday: {
+    officeHour: `Open from ${Wednesday.open}am until ${Wednesday.close}pm`,
+    exhibition: species.reduce((acc, specie) => {
+      if (specie.availability.includes('Wednesday')) acc.push(specie.name);
+      return acc;
+    }, []),
+  },
+  Thursday: {
+    officeHour: `Open from ${Thursday.open}am until ${Thursday.close}pm`,
+    exhibition: species.reduce((acc, specie) => {
+      if (specie.availability.includes('Thursday')) acc.push(specie.name);
+      return acc;
+    }, []),
+  },
+  Friday: {
+    officeHour: `Open from ${Friday.open}am until ${Friday.close}pm`,
+    exhibition: species.reduce((acc, specie) => {
+      if (specie.availability.includes('Friday')) acc.push(specie.name);
+      return acc;
+    }, []),
+  },
+  Saturday: {
+    officeHour: `Open from ${Saturday.open}am until ${Saturday.close}pm`,
+    exhibition: species.reduce((acc, specie) => {
+      if (specie.availability.includes('Saturday')) acc.push(specie.name);
+      return acc;
+    }, []),
+  },
+  Sunday: {
+    officeHour: `Open from ${Sunday.open}am until ${Sunday.close}pm`,
+    exhibition: species.reduce((acc, specie) => {
+      if (specie.availability.includes('Sunday')) acc.push(specie.name);
+      return acc;
+    }, []),
+  },
+  Monday: {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  },
+};
+
+function getSchedule(str) {
+  if (!str) return schedule;
+  if (species.filter((specie) => specie.name.includes(str))) {
+    return species.find(({ name }) => name.includes(str)).availability;
+  }
 }
+
+// console.log(getSchedule('Tuesday'));
 
 module.exports = getSchedule;
